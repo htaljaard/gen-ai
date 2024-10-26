e6e5cd39f6937695919137332723aaa95d20975c
Hein Taljaard <hein.taljaard@needcoffee.net>
2024-10-26 20:57:28 +1100

    Added SK projects.

---
+++ SKYouTubeDemo/Getting_Started/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var endPoint = config["endpoint"];
+var key = config["apiKey"];
+var deploymentName = config["deployment"];
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddAzureOpenAIChatCompletion(deploymentName!, endPoint!, key!);
+
+var kernel = builder.Build();
+
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+
+var history = new ChatHistory("You are a useful AI assistant");
+
+while (true)
+{
+    Console.Write("User: ");
+    var input = Console.ReadLine();
+    history.AddUserMessage(input!);
+
+    ChatMessageContent response = await chat.GetChatMessageContentAsync(
+        chatHistory: history,
+        kernel: kernel
+    );
+
+    Console.WriteLine($"Bot: {response}");
+    history.AddAssistantMessage(response.Content);
+}
