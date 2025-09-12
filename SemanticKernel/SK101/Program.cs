6fd15a71bc62b3739d9962f8acdc041f7b604bca
Hein Taljaard <hein.taljaard@gmail.com>
2025-09-12 20:46:01 +1000

    Add Semantic Kernel project structure and initial implementation with Azure OpenAI integration

---
+++ SemanticKernel/SK101/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>() // or your main class type
+    .Build();
+
+string AOIKEY = config["AOIKEY"]!;
+string AOIENDPOINT = config["AOIENDPOINT"]!;
+
+var kernel = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion("gpt-4o", AOIENDPOINT, AOIKEY)
+    .Build();
+    
+#region Basic Invocation
+
+    var prompt = @"What is the capital of France?";
+    
+    var response = await kernel.InvokePromptAsync(prompt);
+    
+    Console.WriteLine(response);
+
+#endregion
+
+
